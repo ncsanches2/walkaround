dc036bc9f5d800cc875164b2fb311a46cfc2ff0c
ncsanches2 <68040472+ncsanches2@users.noreply.github.com>
2020-07-08 20:18:05 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -17,6 +17,7 @@ let g_rightTrim = 0
 
 const c_backupMelody = ["C6:3", "R:3"]
 const c_scanMelody = ["C3:1", "R:1", "C3:1", "R:1"]
+const c_setupComplete = ["C5:1", "R:1", "C5:1", "R:1", "C5:1", "R:1"]
 
 let pixel = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 
@@ -24,7 +25,6 @@ basic.forever(function () {
 	let exit = GoForward(c_maxSpeed, 0)
     switch(exit) {
         case ForwardExit.Distance:
-            // GoForward(c_slowSpeed, 200)
             Scan()
             break
         
@@ -34,9 +34,45 @@ basic.forever(function () {
     }
 })
 
+function TrySetup()
+{
+    const c_setupTimeout = 10000
+    let startTime = input.runningTime()
+    let btnA = input.buttonIsPressed(Button.A)
+    let btnB = input.buttonIsPressed(Button.B)
+
+    if(!btnA && !btnB)
+    {
+        return
+    }
+
+    MotorStop()
+    pixel.showColor(neopixel.rgb(0,0,0))
+
+    do {
+        if(btnA && btnB) {
+            g_leftTrim = g_rightTrim = 0
+            basic.showString("Reset")
+            let startTime = input.runningTime()
+        } else if(btnA) {
+            g_rightTrim = Math.min(g_rightTrim + 1, c_maxSpeed)
+            basic.showNumber(c_maxSpeed - g_rightTrim)  
+            let startTime = input.runningTime()      
+        } else if(btnB) {
+            g_leftTrim = Math.min(g_leftTrim + 1, c_maxSpeed)
+            basic.showNumber(c_maxSpeed - g_leftTrim)
+            let startTime = input.runningTime()
+        }
+    } while (input.runningTime() - startTime < c_setupTimeout)
+
+    music.startMelody(c_scanMelody, MelodyOptions.Once)
+    basic.pause(2000)
+}
+
 function GoForward(speed: number, distanceInterval: number)
 {
 	while (true) {
+        TrySetup()
 		let dist = GetDist(5, distanceInterval)
 		maqueen.readPatrol(maqueen.Patrol.PatrolLeft)
 	
@@ -58,7 +94,17 @@ function GoForward(speed: number, distanceInterval: number)
 
 function MotorRun(motor: maqueen.Motors, dir: maqueen.Dir, speed: number)
 {
-    maqueen.motorRun(motor, dir, speed)
+    if(motor == maqueen.Motors.All &&
+        dir == maqueen.Dir.CW &&
+        (g_leftTrim > 0 || g_rightTrim > 0)) {
+        
+        maqueen.motorRun(maqueen.Motors.M1,dir, speed - g_leftTrim)
+        maqueen.motorRun(maqueen.Motors.M2, dir, speed - g_rightTrim)
+    }
+    else
+    {
+        maqueen.motorRun(motor, dir, speed)
+    }
 }
 
 function MotorStop()
@@ -91,6 +137,8 @@ function Scan()
 	let time = timeIncrement
 
 	while(true) {
+        TrySetup()
+
 		g_scanDir *= -1
 		Rotate(g_scanDir, time)
 		let dist = GetDist(10, 50)
@@ -122,6 +170,8 @@ function Backtrack()
 
     let motor = null
     while(true) {
+        TrySetup()
+
         let left = maqueen.readPatrol(maqueen.Patrol.PatrolLeft);
         let right = maqueen.readPatrol(maqueen.Patrol.PatrolRight);
 
